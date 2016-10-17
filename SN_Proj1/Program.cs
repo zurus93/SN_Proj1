55aa2c20afe9acf5bb9249d080bb330dfdcaff41
zurus93 <zurus93@gmail.com>
2016-10-17 18:23:53 +0200

    Neural Network - create project.

--- SN_Proj1/Program.cs
+++ SN_Proj1/Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Engine.Network.Activation;
+using Encog.ML.Data;
+using Encog.Neural.Networks.Training.Propagation.Resilient;
+using Encog.ML.Train;
+using Encog.ML.Data.Basic;
+using Encog;
 namespace SN_Proj1
 {
     class Program
