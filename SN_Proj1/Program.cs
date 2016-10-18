6b99695e9ce4feae078676eb037dac5edc7393fe
zurus93 <zurus93@gmail.com>
2016-10-18 20:38:23 +0200

    saving/loading network + testing

--- SN_Proj1/Program.cs
+++ SN_Proj1/Program.cs
@@ -1,70 +1,99 @@
 ﻿using Encog.Neural.Networks;
 using Encog.Neural.Networks.Layers;
 using Encog.Engine.Network.Activation;
-using Encog.ML.Data;
-using Encog.Neural.Networks.Training.Propagation.Resilient;
-using Encog.ML.Train;
-using Encog.ML.Data.Basic;
-using Encog;using System;
-using System.Collections;
+using Encog.ML.Data;using System;
 using System.Collections.Generic;
 using System.Linq;
 using Encog.Neural.Networks.Training.Propagation.Back;
 using Encog.Util.CSV;
 using Encog.Neural.Data.Basic;
-using Encog.ML.Data.Versatile.Sources;
-using Encog.ML.Data.Versatile;
-using Encog.ML.Data.Versatile.Columns;
-using Encog.ML.Data.Versatile.Normalizers.Strategy;
-using Encog.ML.Model;
-using Encog.ML.Factory;
 using Encog.Util.Arrayutil;
-using Encog.Util.Normalize.Output.Nominal;
-using Encog.Util.Simple;
+using Encog.Util;
 
 namespace SN_Proj1
 {
     class Program
     {
+        private static String FILENAME = "data.xsq.train.csv";
+        private static String TEST_FILENAME = "data.xsq.test.csv";
         private static int LAYERS = 4;
         private static int NEURONS = 30;
         private static int ITERATIONS = 100;
-        private static double LEARNING_RATE = 0.2;
-        private static double MOMENTUM = 0.2;
+        private static double LEARNING_RATE = 0.7;
+        private static double MOMENTUM = 0.3;
+
+        private static BasicNetwork _network;
 
         static void Main(string[] args)
         {
-            var trainingSet = readCSV("data.xsq.train.csv");
+            var trainingSet = readCSV(FILENAME, false);
+            var testingSet = readCSV(TEST_FILENAME, true);
 
-            var network = new BasicNetwork();
-            network.AddLayer(new BasicLayer(1));
+            try
+            {
+                _network = (BasicNetwork)SerializeObject.Load(FILENAME + ".ser");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                _network = new BasicNetwork();
+                initializeNetwork(_network, trainingSet.InputSize, trainingSet.IdealSize);
+            }                      
+
+            Console.WriteLine("Press any key to start");
+            Console.WriteLine("Press ESC to stop");           
+            Console.ReadKey();
+
+            trainNetwork(trainingSet);
+            SerializeObject.Save(FILENAME + ".ser", _network); 
+
+            Console.WriteLine("Press any key to start testing");
+            Console.ReadKey();
+           
+            testNetwork(testingSet);
+
+            Console.WriteLine("Press any key to leave");
+            Console.ReadKey();
+        }
+
+        private static void testNetwork(IMLDataSet testingSet)
+        {
+            foreach (IMLDataPair pair in testingSet)
+            {
+                IMLData output = _network.Compute(pair.Input);
+                Console.WriteLine(@"Computet result = " + output[0]);
+            }
+        }
+
+        private static void trainNetwork(IMLDataSet trainingSet)
+        {
+            var train = new Backpropagation(_network, trainingSet, LEARNING_RATE, MOMENTUM);
+
+            int epoch = 1;
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    train.Iteration();
+                    Console.WriteLine(@"Epoch #" + epoch + @" Error: " + train.Error);
+                    epoch++;
+                }
+            } while (train.Error > 0.004 & Console.ReadKey(true).Key != ConsoleKey.Escape);
+            train.FinishTraining();
+        }
+
+        private static void initializeNetwork(BasicNetwork network, int inputSize, int idealSize)
+        {
+            network.AddLayer(new BasicLayer(inputSize));
 
             for (int i = 0; i < LAYERS; ++i)
             {
                 network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, NEURONS));
             }
 
-            network.AddLayer(new BasicLayer(1));
+            network.AddLayer(new BasicLayer(idealSize));
             network.Structure.FinalizeStructure();
             network.Reset();
-
-            var train = new Backpropagation(network, trainingSet);
-
-            int epoch = 1;
-            do
-            {
-                train.Iteration();
-                Console.WriteLine(@"Epoch #" + epoch + @" Error: " + train.Error);
-                epoch++;
-            } while (train.Error > 0.004);
-            train.FinishTraining();
-
-            Console.ReadKey();
         }
-
-
-
-
 
         private static double[] ReadLine(ReadCSV csv)
         {
@@ -75,7 +104,6 @@ namespace SN_Proj1
             }
             return line;
         }
-
 
         private static double[][] LoadCSV(ReadCSV csvReader)
         {
@@ -93,19 +121,29 @@ namespace SN_Proj1
             return data.ToArray();
         }
 
-        private static IMLDataSet readCSV(string path)
+        private static IMLDataSet readCSV(string path, bool test)
         {
             var reader = new ReadCSV(path, true, CSVFormat.DecimalPoint);
             var data = LoadCSV(reader);
             data = Normalize(data);
             reader.Close();
 
-            var input = data.Select(row => row.Take(row.Length - 1).ToArray()).ToArray();
-            var ideal = data.Select(row => row.Skip(row.Length - 1).ToArray()).ToArray(); // last value
+            double[][] input;
+            double[][] ideal;
+
+            if (test)
+            {
+                input = data; // testing set doesn't have 'ideal' row
+                ideal = data;
+            }
+            else
+            {
+                input = data.Select(row => row.Take(row.Length - 1).ToArray()).ToArray();
+                ideal = data.Select(row => row.Skip(row.Length - 1).ToArray()).ToArray(); // last value
+            }
 
             return new BasicNeuralDataSet(input, ideal);
         }
-
 
         private static double[][] Normalize(double[][] data)
         {
@@ -132,9 +170,7 @@ namespace SN_Proj1
                 }
             }
 
-
             return result;
-
         }
     }
 }
