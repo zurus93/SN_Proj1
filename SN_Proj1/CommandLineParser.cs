eb161f0486b7f6a4900df362e4ba75852e601d2c
Marcin Wardzinski <wardzinskim@outlook.com>
2016-10-25 01:07:10 +0200

    command line parser

---
+++ SN_Proj1/CommandLineParser.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN_Proj1
+{
+    public class UnrecognizedArgumentException : ApplicationException
+    {
+    }
+
+    public class MultipleArgumentAssignmentException : ApplicationException
+    {
+    }
+
+    public class CommandLineOption
+    {
+        public char ShortNotation { get; set; }
+        public string LongNotation { get; set; }
+        public bool ParameterRequired { get; set; }
+    }
+
+
+    /// <summary>
+    /// Parser linii poleceń, weryfikuje ją i zwraca zarejestrowane polecenia w wygodnej formie.
+    /// </summary>
+    public class CommandLineParser
+    {
+        private List<CommandLineOption> _options;
+        private Dictionary<CommandLineOption, string> _settings;
+
+        public CommandLineParser(ICollection<CommandLineOption> options)
+        {
+            _options = options.ToList();
+            _settings = new Dictionary<CommandLineOption, string>();
+        }
+
+        public void Parse(string[] arguments)
+        {
+            for (int parsedArguments = 0; parsedArguments < arguments.Length; ++parsedArguments)
+            {
+                var argument = arguments[parsedArguments];
+
+                if (argument.StartsWith("-"))
+                {
+                    var option = _options.FirstOrDefault(t => argument == "-" + t.LongNotation);
+
+                    if (option == null)
+                    {
+                        throw new UnrecognizedArgumentException();
+                    }
+
+                    if (_settings.ContainsKey(option))
+                    {
+                        throw new MultipleArgumentAssignmentException();
+                    }
+
+                    if (option.ParameterRequired && (parsedArguments + 1 >= arguments.Length))
+                    {
+                        throw new Exception();
+                    }
+
+                    string value = null;
+                    if (option.ParameterRequired)
+                    {
+                        value = arguments[parsedArguments + 1];
+                        ++parsedArguments;
+                    }
+
+                    _settings.Add(option, value);
+
+                }
+                else
+                {
+                    for (int i = 0; i < argument.Length; ++i)
+                    {
+                        var shortOpt = argument[i];
+                        var option = _options.First(t => shortOpt == t.ShortNotation);
+                        if (option.ParameterRequired && i != argument.Length - 1)
+                            throw new Exception();
+                        string value = null;
+                        if (option.ParameterRequired)
+                        {
+                            value = arguments[parsedArguments + 1];
+                            ++parsedArguments;
+                        }
+                        _settings.Add(option, value);
+                    }
+                }
+            }
+        }
+
+        public bool TryGet(string key, out string value)
+        {
+            var option = _options.First(t => t.LongNotation == key);
+            if (!_settings.ContainsKey(option))
+            {
+                value = null;
+                return false;
+            }
+
+            value = _settings[option];
+            return true;
+        }
+    }
+
+}
--- SN_Proj1/Program.cs
+++ SN_Proj1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Encog.Neural.Networks.Training.Simple;
 
 namespace SN_Proj1
 {
@@ -13,47 +15,51 @@ namespace SN_Proj1
 
         static void Main(string[] args)
         {
+            var settings = ApplyArguments(args);
+
             var trainingData = CsvHelper.Read(FILENAME);
             var validationData = CsvHelper.Read(TEST_FILENAME);
 
-            var settings = new NeuralSettings
-            {
-                HasBias = true,
-                ActivationFunction = ActivationFunction.Bipolar,
-                HiddenLayers = new[] { 40, 30 },
-                LearningRate = 0.003,
-                Momentum = 0.03,
-                Iterations = 200,
-                Type = ProblemType.Classification
-            };
 
             // Zakładamy, że liczba elementów w każdej kolumnie jest taka sama i że w ostatniej kolumnie
             // znajduje się wynik
             int inputSize = trainingData[0].Length - 1;
-            int idealSize = settings.Type == ProblemType.Regression ? 1 : CLUSTERS_COUNT;
+            int idealSize = settings.Type == ProblemType.Regression ? 1 : GetClastersCount(trainingData);
 
-            var testData = validationData.Select(x => x.Take(2).ToArray()).ToArray();
+            var testData = validationData.Select(x => x.Take(inputSize).ToArray()).ToArray();
+
+
+
             var neuralWrapper = new NeuralWrapper(settings, inputSize, idealSize);
             neuralWrapper.BuildNetwork();
             var error = neuralWrapper.Train(trainingData, validationData);
 
             var result = neuralWrapper.Test(testData);
 
-            CsvHelper.Write("sortedInput.csv", SortByColumn(trainingData, 0));
-            CsvHelper.Write("output.csv", testData, result);
-            CsvHelper.Write("error.csv", error);
+            if (settings.Type == ProblemType.Regression)
+            {
+
+                CsvHelper.Write("sortedInput.csv", SortByColumn(trainingData, 0));
+                CsvHelper.Write("output.csv", testData, result);
+                CsvHelper.Write("error.csv", error);
 
 
-            new GnuplotScriptRunner(@"gnuplot/regression.gnu")
-                .AddScriptParameter("trainingSet", Path.GetFullPath("sortedInput.csv"))
-                .AddScriptParameter("testSet", Path.GetFullPath("output.csv"))
-                .Run();
+                new GnuplotScriptRunner(@"gnuplot/regression.gnu")
+                    .AddScriptParameter("trainingSet", Path.GetFullPath("sortedInput.csv"))
+                    .AddScriptParameter("testSet", Path.GetFullPath("output.csv"))
+                    .Run();
 
-            new GnuplotScriptRunner(@"gnuplot/networkError.gnu")
-                .AddScriptParameter("input", Path.GetFullPath("error.csv"))
-                .Run();
+                new GnuplotScriptRunner(@"gnuplot/networkError.gnu")
+                    .AddScriptParameter("input", Path.GetFullPath("error.csv"))
+                    .Run();
 
+            }
             Console.ReadLine();
+        }
+
+        public static int GetClastersCount(double[][] data)
+        {
+            return (int)data.Select(x => x.Last()).Max();
         }
 
         public static double[][] SortByColumn(double[][] data, int column)
@@ -68,6 +74,63 @@ namespace SN_Proj1
             }
 
             return data.Select(x => x).OrderByDescending(x => x[column]).ToArray();
+        }
+
+        public static NeuralSettings ApplyArguments(string[] arguments)
+        {
+            var parser = new CommandLineParser(new List<CommandLineOption>
+            {
+                new CommandLineOption {ShortNotation = 'i', LongNotation = "iterations", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'h', LongNotation = "hidden-layers", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 't', LongNotation = "problem-type", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'f', LongNotation = "activation-function", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'b', LongNotation = "bias", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'l', LongNotation = "learning-rate", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'm', LongNotation = "momentum", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'd', LongNotation = "training-data", ParameterRequired = true},
+                new CommandLineOption {ShortNotation = 'v', LongNotation = "test-data", ParameterRequired = true}
+            });
+
+            parser.Parse(arguments);
+            var settings = new NeuralSettings { HiddenLayers = new int[0], HasBias = true };
+
+            string value = null;
+            if (parser.TryGet("iterations", out value))
+                settings.Iterations = int.Parse(value);
+
+            if (parser.TryGet("momentum", out value))
+                settings.Momentum = double.Parse(value);
+
+            if (parser.TryGet("learning-rate", out value))
+                settings.LearningRate = double.Parse(value);
+
+            if (parser.TryGet("bias", out value))
+                settings.HasBias = bool.Parse(value);
+
+            if (parser.TryGet("activation-function", out value))
+            {
+                ActivationFunction tmp;
+                Enum.TryParse(value, true, out tmp);
+                settings.ActivationFunction = tmp;
+            }
+
+            if (parser.TryGet("problem-type", out value))
+            {
+                ProblemType tmp;
+                Enum.TryParse(value, true, out tmp);
+                settings.Type = tmp;
+            }
+
+            if (parser.TryGet("hidden-layers", out value))
+                settings.HiddenLayers = value.Split(',').Select(int.Parse).ToArray();
+
+            if (parser.TryGet("training-data", out value))
+                FILENAME = value;
+
+            if (parser.TryGet("test-data", out value))
+                TEST_FILENAME = value;
+
+            return settings;
         }
     }
 }
